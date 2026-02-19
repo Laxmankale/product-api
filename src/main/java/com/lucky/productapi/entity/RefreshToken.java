df8e467a2c34977d23de18b12ab3bcf6d0585f2a
Laxman Kale <lakhankale888@gmail.com>
2026-02-20 00:33:55 +0530

    Add User and RefreshToken entities with repositories

---
+++ src/main/java/com/lucky/productapi/entity/RefreshToken.java
@@ -0,0 +1,30 @@
+package com.lucky.productapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_token")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String token;
+
+	@Column(nullable = false)
+	private LocalDateTime expiryDate;
+
+	@OneToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+}
