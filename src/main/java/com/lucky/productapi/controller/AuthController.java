b74f62f98db861ed57b45fee008cb9d68ea69e8c
Laxman Kale <lakhankale888@gmail.com>
2026-02-20 12:16:11 +0530

    Modify login endpoint to generate JWT token

--- src/main/java/com/lucky/productapi/controller/AuthController.java
+++ src/main/java/com/lucky/productapi/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.lucky.productapi.controller;
 
 import com.lucky.productapi.dto.LoginRequest;
+import com.lucky.productapi.entity.User;
+import com.lucky.productapi.repository.UserRepository;
+import com.lucky.productapi.security.JwtUtil;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
@@ -10,25 +14,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+	private final AuthenticationManager authenticationManager;
+	private final JwtUtil jwtUtil;
+	private final UserRepository userRepository;
 
-    private final AuthenticationManager authenticationManager;
-
-    public AuthController(AuthenticationManager authenticationManager) {
+	public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserRepository userRepository) {
 		super();
 		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+		this.userRepository = userRepository;
 	}
 
 	@PostMapping("/login")
-    public String login(@RequestBody LoginRequest request) {
+	public String login(@RequestBody LoginRequest request) {
 
-        Authentication authentication =
-                authenticationManager.authenticate(
-                        new UsernamePasswordAuthenticationToken(
-                                request.getUsername(),
-                                request.getPassword()
-                        )
-                );
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 
-        return "Login successful for user: " + authentication.getName();
-    }
+		User user = userRepository.findByUsername(authentication.getName()).orElseThrow();
+
+		return jwtUtil.generateToken(user.getUsername(), user.getRole());
+	}
 }
